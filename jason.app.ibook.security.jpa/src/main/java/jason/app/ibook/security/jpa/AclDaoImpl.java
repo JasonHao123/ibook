178664142568a8768f5374b11d10772c51c0222c
Jason Hao <nk.smallbee@gmail.com>
2015-05-22 16:31:17 +0800

    fix sql identifer case to upper case

--- jason.app.ibook.security.jpa/src/main/java/jason/app/ibook/security/jpa/AclDaoImpl.java
+++ jason.app.ibook.security.jpa/src/main/java/jason/app/ibook/security/jpa/AclDaoImpl.java
@@ -40,11 +40,33 @@ public class AclDaoImpl implements IAclDao {
             + "acli_sid.principal as acl_principal, "
             + "acli_sid.sid as acl_sid, "
             + "acl_class.class "
-            + "from acl_object_identity "
+            + "from acl_object_identity"
             + "left join acl_sid acli_sid on acli_sid.id = acl_object_identity.owner_sid "
             + "left join acl_class on acl_class.id = acl_object_identity.object_id_class   "
             + "left join acl_entry on acl_object_identity.id = acl_entry.acl_object_identity "
             + "left join acl_sid on acl_entry.sid = acl_sid.id  "
+            + "where ( ";
+    
+    public final static String DEFAULT_SELECT_CLAUSE_UPPER = "select acl_object_identity.object_id_identity, "
+            + "acl_entry.ace_order,  "
+            + "acl_object_identity.id as acl_id, "
+            + "acl_object_identity.parent_object, "
+            + "acl_object_identity.entries_inheriting, "
+            + "acl_entry.id as ace_id, "
+            + "acl_entry.mask,  "
+            + "acl_entry.granting,  "
+            + "acl_entry.audit_success, "
+            + "acl_entry.audit_failure,  "
+            + "acl_sid.principal as ace_principal, "
+            + "acl_sid.sid as ace_sid,  "
+            + "acli_sid.principal as acl_principal, "
+            + "acli_sid.sid as acl_sid, "
+            + "acl_class.class "
+            + "from ACL_OBJECT_IDENTITY acl_object_identity"
+            + "left join ACL_SID acli_sid on acli_sid.id = acl_object_identity.owner_sid "
+            + "left join ACL_CLASS acl_class on acl_class.id = acl_object_identity.object_id_class   "
+            + "left join ACL_ENTRY acl_entry on acl_object_identity.id = acl_entry.acl_object_identity "
+            + "left join ACL_SID acl_sid on acl_entry.sid = acl_sid.id  "
             + "where ( ";
 
         private final static String DEFAULT_LOOKUP_KEYS_WHERE_CLAUSE = "(acl_object_identity.id = ?)";
@@ -55,7 +77,7 @@ public class AclDaoImpl implements IAclDao {
             + " asc, acl_entry.ace_order asc";
         
         // SQL Customization fields
-        private String selectClause = DEFAULT_SELECT_CLAUSE;
+        private String selectClause = DEFAULT_SELECT_CLAUSE_UPPER;
         private String lookupPrimaryKeysWhereClause = DEFAULT_LOOKUP_KEYS_WHERE_CLAUSE;
         private String lookupObjectIdentitiesWhereClause = DEFAULT_LOOKUP_IDENTITIES_WHERE_CLAUSE;
         private String orderByClause = DEFAULT_ORDER_BY_CLAUSE;
